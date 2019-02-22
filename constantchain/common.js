574566cbb32e2c742b3c0c92528c9c7ab3aa69bc
0xsirrush <thaibao@autonomous.nyc>
2019-02-22 10:13:17 +0700

    - update

---
+++ constantchain/common.js
@@ -0,0 +1,10 @@
+import * as constants from "privacy-js-lib/lib/constants"
+
+/**
+ * @return {number}
+ */
+function getShardIDFromLastByte(lastByte) {
+  return lastByte % constants.SHARD_NUMBER
+}
+
+module.exports = {getShardIDFromLastByte};
