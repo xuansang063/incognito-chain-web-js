3fe7f8dc08f8253d9d94308026f0db709885aada
0xthunderbird <dat@ninja.org>
2019-03-04 11:15:13 +0700

    update

--- test/wallet/wallet-test.js
+++ test/wallet/wallet-test.js
@@ -1,4 +1,7 @@
 import {Wallet, DefaultStorage} from '../../lib/wallet/wallet'
+import {KeyWallet as keyWallet} from "../../lib/wallet/hdwallet";
+import * as key from "../../lib/key";
+import bn from 'bn.js';
 
 async function Test() {
   // let ID  = "1AF5782F86BDA63F884C7D8F872FF135A6F567FC0932DA3A675ECB2DD344DA40";
@@ -32,7 +35,17 @@ async function TestWallet() {
   let wallet = new Wallet();
   wallet.init("12345678", 0, "Wallet", storage);
   wallet.importAccount(priK, "Dat", "12345678");
-  let balance = await wallet.masterAccount.child[1].getBalance();
+  let n = 1;
+  let paymentInfos = new Array(n);
+
+  //HN2
+  let receiverSpendingKeyStr1 = "112t8rqnMrtPkJ4YWzXfG82pd9vCe2jvWGxqwniPM5y4hnimki6LcVNfXxN911ViJS8arTozjH4rTpfaGo5i1KKcG1ayjiMsa4E3nABGAqQh";
+  let receiverKeyWallet1 = keyWallet.base58CheckDeserialize(receiverSpendingKeyStr1);
+  // import key set
+  receiverKeyWallet1.KeySet.importFromPrivateKey(receiverKeyWallet1.KeySet.PrivateKey);
+  paymentInfos[0] = new key.PaymentInfo(receiverKeyWallet1.KeySet.PaymentAddress, new bn(2300));
+  wallet.MasterAccount.child[1].createAndSendConstant(paymentInfos);
+  let balance = await wallet.MasterAccount.child[1].getBalance();
   console.log(balance)
 }
 
