a5ca742602e17ce1f3d79d30f34fd8f088868666
0xkraken <hien.n@autonomous.nyc>
2019-05-23 18:46:02 +0700

    refactor rpcclient

--- test/utils-test.js
+++ test/utils-test.js
@@ -5,3 +5,15 @@ const res = toConstant(1000);
 
 console.log(res);
 console.log(typeof res);
+
+
+
+function a(){
+    if (1){
+        throw new Error("AAAAAA");
+    }
+
+    console.log("BBBBBBBB");
+}
+
+a()
