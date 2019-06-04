1c76b6a699382c5026819c08c2318f28ef605daa
0xkraken <hien.n@autonomous.nyc>
2019-06-04 15:18:49 +0700

    fix bug

--- test/bigint-test.js
+++ test/bigint-test.js
@@ -1,100 +1,109 @@
 import bn from 'bn.js';
 import bigInt from 'big-integer'
 
-let ECC = require('elliptic');
-let Elliptic = ECC.ec;
+// let ECC = require('elliptic');
+// let Elliptic = ECC.ec;
 
-function modInverse(a, m) {
-  let m0 = m;
-  let y = BigInt("0")
-  let x = BigInt("1");
+// function modInverse(a, m) {
+//   let m0 = m;
+//   let y = BigInt("0")
+//   let x = BigInt("1");
 
-  if (m == 1)
-    return 0;
+//   if (m == 1)
+//     return 0;
 
-  while (a > 1) {
-    // q is quotient
-    let q = a / m;
-    let t = m;
+//   while (a > 1) {
+//     // q is quotient
+//     let q = a / m;
+//     let t = m;
 
-    // m is remainder now, process same as
-    // Euclid's algo
-    m = a % m;
-    a = t;
-    t = y;
+//     // m is remainder now, process same as
+//     // Euclid's algo
+//     m = a % m;
+//     a = t;
+//     t = y;
 
-    // Update y and x
-    y = x - q * y;
-    x = t;
-  }
+//     // Update y and x
+//     y = x - q * y;
+//     x = t;
+//   }
 
-  // Make x positive
-  if (x < 0)
-    x = x + m0;
+//   // Make x positive
+//   if (x < 0)
+//     x = x + m0;
 
-  return x;
-}
+//   return x;
+// }
 
-function power(base, power, n) {
-  let result = BigInt(1);
-  const twoBN = BigInt(2);
+// function power(base, power, n) {
+//   let result = BigInt(1);
+//   const twoBN = BigInt(2);
 
-  while (power > 0) {
-    // if power id odd
-    if (power % twoBN == 1) {
-      result = (result * base) % n;
-    }
+//   while (power > 0) {
+//     // if power id odd
+//     if (power % twoBN == 1) {
+//       result = (result * base) % n;
+//     }
 
-    power = power / twoBN;
-    base = (base * base) % n;
-  }
+//     power = power / twoBN;
+//     base = (base * base) % n;
+//   }
 
-  return result;
-}
+//   return result;
+// }
 
-let n = bn.red((new Elliptic('p256')).n.clone())
-let a = new bn("1")
-let b = new bn("2")
-console.time("1")
-for (let i = 0; i < 100; i++) {
-  let c = a.toRed(n).redAdd(b.toRed(n))
-  let temp3 = c.redInvm().fromRed()
-  // console.log(temp3.toString())
-}
-console.timeEnd("1")
+// let n = bn.red((new Elliptic('p256')).n.clone())
+// let a = new bn("1")
+// let b = new bn("2")
+// console.time("1")
+// for (let i = 0; i < 100; i++) {
+//   let c = a.toRed(n).redAdd(b.toRed(n))
+//   let temp3 = c.redInvm().fromRed()
+//   // console.log(temp3.toString())
+// }
+// console.timeEnd("1")
 
-console.time("2")
-for (let i = 0; i < 100; i++) {
-  let c = bigInt(a.toString()).plus(bigInt(b.toString()))
-  let temp3 = c.modInv(bigInt((new Elliptic('p256')).n.toString()))
-  // console.log(temp3.toString())
-}
-console.timeEnd("2")
+// console.time("2")
+// for (let i = 0; i < 100; i++) {
+//   let c = bigInt(a.toString()).plus(bigInt(b.toString()))
+//   let temp3 = c.modInv(bigInt((new Elliptic('p256')).n.toString()))
+//   // console.log(temp3.toString())
+// }
+// console.timeEnd("2")
 
-console.time("3")
-a = BigInt(a.toString());
-b = BigInt(b.toString())
-let m = BigInt((new Elliptic('p256')).n.toString());
+// console.time("3")
+// a = BigInt(a.toString());
+// b = BigInt(b.toString())
+// let m = BigInt((new Elliptic('p256')).n.toString());
 
-for (let i = 0; i < 100; i++) {
-  let c = a + b
-  c = modInverse(c, m)
-  // console.log("modInverse(a, m):", c);
-}
-console.timeEnd("3")
+// for (let i = 0; i < 100; i++) {
+//   let c = a + b
+//   c = modInverse(c, m)
+//   // console.log("modInverse(a, m):", c);
+// }
+// console.timeEnd("3")
 
-/*console.time("power")
-let num1 = BigInt(1050);
-let num2 = BigInt(18);
-let modN = BigInt((new Elliptic('p256')).n.clone().toString());
-let res = power(num1, num2, modN);
-console.log("10^20 = ", res);
-console.timeEnd("power")*/
+// /*console.time("power")
+// let num1 = BigInt(1050);
+// let num2 = BigInt(18);
+// let modN = BigInt((new Elliptic('p256')).n.clone().toString());
+// let res = power(num1, num2, modN);
+// console.log("10^20 = ", res);
+// console.timeEnd("power")*/
 
 
-let h = [167,2,34,4];
-let g = new bn(h);
-console.log("b.toNumber(): ", g.toNumber());
+// let h = [167,2,34,4];
+// let g = new bn(h);
+// console.log("b.toNumber(): ", g.toNumber());
+
+
+let hhhhh = new bn(499989997);
+console.log("HHHHH ", hhhhh.toString());
+
+
+let ggggg = new bn(parseInt("499989997"));
+console.log(ggggg.toString());
+
 
 
 
