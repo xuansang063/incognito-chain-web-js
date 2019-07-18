624a3c05564000ea4b32d84c9b7cf4b895c0d154
0xkraken <hien.n@autonomous.nyc>
2019-07-18 16:22:44 +0700

    convert unit of currency

--- test/utils-test.js
+++ test/utils-test.js
@@ -1,19 +1,2 @@
 
-import {toConstant} from '../lib/wallet/utils';
 
-const res = toConstant(1000);
-
-console.log(res);
-console.log(typeof res);
-
-
-
-function a(){
-    if (1){
-        throw new Error("AAAAAA");
-    }
-
-    console.log("BBBBBBBB");
-}
-
-a()
