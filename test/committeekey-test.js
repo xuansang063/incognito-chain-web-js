bbca2a960dd7b17b02a69b2c11c16e519e5affa5
0xsirrush <thaibao@autonomous.nyc>
2019-08-21 23:17:31 +0700

    - test wasm

--- test/committeekey-test.js
+++ test/committeekey-test.js
@@ -2,10 +2,19 @@ import { generateCommitteeFromHashPrivateKey} from "../lib/committeekey";
 import {KeyWallet} from "../lib/wallet/hdwallet";
 import {checkDecode} from "../lib/base58";
 
+async function sleep(sleepTime) {
+    return new Promise(resolve => setTimeout(resolve, sleepTime));
+}
 
-function testCommitteeKey(){
+async function testCommitteeKey(){
     let candidateHashPrivateKey = "1TQxX6EFXRcU2FUQmbWozSRtPsgbFutWZbeBSM5jrk2mYGQYEz";
     let candidatePaymentAddress = "1Uv4ZQWnE2j832G4NDkceANsMpEFZWHaGtquKzgYdw2XKS1Zrvre5Bxfr4eBUm87Xt7vdYakjuXmfKyEXkJVpo3fJR2fM2nhHiEiGPuKr";
+
+    console.log("Wait for WASM be started")
+    await sleep(3000)
+    // let a = await add(1,4)
+    // console.log(a)
+    // return ;
 
     // generate committee key
     let keyWallet = KeyWallet.base58CheckDeserialize(candidatePaymentAddress);
@@ -13,7 +22,8 @@ function testCommitteeKey(){
 
     let candidateHashPrivateKeyBytes = checkDecode(candidateHashPrivateKey).bytesDecoded;
 
-    let committeeKey = generateCommitteeFromHashPrivateKey(candidateHashPrivateKeyBytes, publicKeyBytes);
+    console.log("generateCommitteeFromHashPrivateKey")
+    let committeeKey = await generateCommitteeFromHashPrivateKey(candidateHashPrivateKeyBytes, publicKeyBytes);
     console.log("committeeKey: ", committeeKey);
 }
 
