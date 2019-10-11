6d8bd930c4f8bd1fe9518f05e72f84a1590026dc
0xkraken <hien.n@autonomous.nyc>
2019-10-11 15:25:31 +0700

    get max amount withdraw ptoken

--- test/wallet/utils-test.js
+++ test/wallet/utils-test.js
@@ -3,7 +3,7 @@ import { AccountWallet, Wallet } from "../../lib/wallet/wallet";
 import { RpcClient } from "../../lib/rpcclient/rpcclient";
 import { CustomTokenInit, CustomTokenTransfer } from "../../lib/tx/constants";
 import { PaymentAddressType } from "../../lib/wallet/constants";
-import { getEstimateFee, getEstimateFeeForSendingToken } from "../../lib/tx/utils";
+import { getEstimateFee, getEstimateFeeForSendingToken, getMaxWithdrawAmount } from "../../lib/tx/utils";
 
 const rpcClient = new RpcClient("https://dev-test-node.incognito.org");
 // const rpcClient = new RpcClient("http://localhost:9334");
@@ -75,4 +75,47 @@ async function TestGetEstimateFeeForSendingToken() {
   console.log("fee: ", fee);
 }
 
-TestGetEstimateFeeForSendingToken();
+// TestGetEstimateFeeForSendingToken();
+
+async function TestGetMaxWithdrawAmount() {
+  Wallet.RpcClient = rpcClient;
+  await sleep(5000);
+  // sender key (private key)
+  let senderPrivateKeyStr = "112t8rnX7qWSJFCnGBq4YPHYN2D29NmGowC5RSbuDUC8Kg8ywg6GsPda5xRJMAmzmVKwLevdJNi5XfrqHRWDzSGEg37kbsrcWrAEQatR1UQQ";
+  let senderKeyWallet = keyWallet.base58CheckDeserialize(senderPrivateKeyStr);
+  senderKeyWallet.KeySet.importFromPrivateKey(senderKeyWallet.KeySet.PrivateKey);
+
+  let accountSender = new AccountWallet();
+  accountSender.key = senderKeyWallet;
+
+  console.log("accountSender: ", accountSender);
+  console.log("accountSender.key: ", accountSender.key);
+
+  let from = "12S4NL3DZ1KoprFRy1k5DdYSXUq81NtxFKdvUTP3PLqQypWzceL5fBBwXooAsX5s23j7cpb1Za37ddmfSaMpEJDPsnJGZuyWTXJSZZ5";
+  let to = "12Ryp47jXJfkz5Cketp4D9U7uTH4hFgFUVUEzq6k5ikvAZ94JucsYbi235siCMud5GdtRi1DoSecsTD2nkiic9TH7YNkLEoEhrvxvwt";
+  let amountTransfer = 100;
+  let isPrivacyForPToken = true;
+  let feeToken = 0;
+
+  //Todo: check with isGetTokenFee = true
+  let isGetTokenFee = true;
+
+  let tokenParams = {
+    Privacy: true,
+    TokenID: "7ff6af1d9e92a572365ffc48a815e2b5cc6ea7d19ad5460df3986ab309439289",
+    TokenName: "Rose 2",
+    TokenSymbol: "Rose 2",
+    TokenTxType: CustomTokenTransfer,
+    TokenAmount: amountTransfer,
+    TokenReceivers: {
+      PaymentAddress: to,
+      Amount: amountTransfer
+    }
+  }
+
+  // customTokenParams = null, privacyTokenParams = null, isGetTokenFee = false
+  let result = await getMaxWithdrawAmount(from, to, tokenParams, accountSender, rpcClient,false , isPrivacyForPToken);
+  console.log("result: ", result);
+}
+
+TestGetMaxWithdrawAmount();
