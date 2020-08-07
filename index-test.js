4d63bbb27008be07e171e7c75cdb0eedec44a8da
0xKraken <hien.n@autonomous.nyc>
2020-08-07 16:06:53 +0700

    Update SDK with new wasm funcs

--- index-test.js
+++ index-test.js
@@ -4,7 +4,7 @@ require("./loadwasm");
 
 // wallet test
 // require("./test/wallet/wallet-test");
-// require("./test/wallet/accountwallet-test");
+require("./test/wallet/accountwallet-test");
 // require("./test/wallet/hdwallet-test");
 // require("./test/wallet/utils-test");
 
@@ -37,7 +37,7 @@ require("./loadwasm");
 // require('./test/txfordev/multiPRVTx');
 // require('./test/txfordev/withdrawReward');
 // require('./test/txfordev/sendRewardsToOneAddress');
-require('./test/txfordev/pTokenContribute');
+// require('./test/txfordev/pTokenContribute');
 // require('./test/txfordev/PRVContribute');
 
 // require('./test/txfordev/sendPrivateTokentoReceivers');
