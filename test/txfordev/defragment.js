cca1714da348531b15d3e07c68abc7c74f604713
0xSiris <siris@incognito.org>
2020-09-07 10:51:10 +0700

    Add defragment PRV

---
+++ test/txfordev/defragment.js
@@ -0,0 +1,28 @@
+import { Wallet } from '../../lib/wallet/wallet'
+import { KeyWallet as keyWallet } from "../../lib/wallet/hdwallet";
+import { AccountWallet } from "../../lib/wallet/accountWallet";
+import { RpcClient } from "../../lib/rpcclient/rpcclient";
+
+Wallet.RpcClient = new RpcClient("https://mainnet.incognito.org/fullnode");
+// Wallet.RpcClient = new RpcClient("https://test-node.incognito.org");
+
+async function sleep(sleepTime) {
+  return new Promise(resolve => setTimeout(resolve, sleepTime));
+}
+
+async function Defragment() {
+  await sleep(8000);
+  // TODO 1. need to fill in your private key
+  const privateKeyStr = "";
+  const senderKeyWallet = keyWallet.base58CheckDeserialize(privateKeyStr);
+  senderKeyWallet.KeySet.importFromPrivateKey(senderKeyWallet.KeySet.PrivateKey);
+
+  let accountSender = new AccountWallet();
+  accountSender.key = senderKeyWallet;
+
+  const fee = 100;
+  const responses = await accountSender.defragmentNativeCoin(fee);
+  console.log("List Tx", responses.map(res => res.txId));
+}
+
+Defragment();
