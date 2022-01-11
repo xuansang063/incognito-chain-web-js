82f45deafcbd020e93454593490bb886096ef878
darius <datle@incognito.org>
2022-01-11 17:36:53 +0700

    update key submission for dev console; revert change to constant

--- wasm/internal/common/constants.go
+++ wasm/internal/common/constants.go
@@ -14,7 +14,7 @@ const (
 	HashSize              = 32 // bytes
 	MaxHashStringSize     = HashSize * 2
 	Base58Version         = 0
-	EncodeCoinsWithBase64 = false
+	EncodeCoinsWithBase64 = true
 )
 
 // size data for incognito key and signature
